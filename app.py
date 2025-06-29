2df8be3364b99f85c7b956ca716910dcc0c9c52d
Arin Vashisth <157912485+arinVashisth@users.noreply.github.com>
2025-06-29 13:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def analyze_resume():
         print("Resume Analysis Error:", e)
         return jsonify({'error': str(e)}), 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
